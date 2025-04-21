92f283eb07497c6b96c86b1c27191de808e840ce
Daniel Neugebauer <dneuge@energiequant.de>
2025-04-21 19:27:09 +0200

    import disclaimer handling and "about" dialog/CLI from legacy proxy

---
+++ src/main/java/de/energiequant/apputils/misc/Constants.java
@@ -0,0 +1,6 @@
+package de.energiequant.apputils.misc;
+
+public class Constants {
+    public static final String DEPENDENCIES_CAPTION = "This software includes the following runtime dependencies.\nFor full author information please refer to their individual websites.\nIn alphabetical order:";
+    public static final String PROJECT_DEPENDENCY_LICENSE_INTRO = "made available under ";
+}
